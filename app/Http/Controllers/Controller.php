855d437cc49e4d399d9e6a9798f3691d83b680c1
unknown <professor404@gmail.com>
2022-03-19 00:12:38 +0300

    users index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,15 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Inertia\Inertia;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(){
+        return Inertia::render('Users/Index', [
+            'users' => Users::all(),
+        ]);
+    }
 }
