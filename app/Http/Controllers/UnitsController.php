8c6b0280d731a83983d066a04ed71db123a65312
unknown <professor404@gmail.com>
2022-03-19 23:21:15 +0300

    init Units

--- app/Http/Controllers/UnitsController.php
+++ app/Http/Controllers/UnitsController.php
@@ -4,82 +4,49 @@ namespace App\Http\Controllers;
 
 use App\Models\Units;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class UnitsController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-        return Inseria::render('Units/Index', [
+        return Inertia::render('Units/Index', [
             'units' => Units::all()
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return Inertia::render('Units/Create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'name' => 'required|string'
+        ],[
+            'name.required' => 'يجب ادخال اسم الوحدة او القسم',
+            'name.string' => 'الاسم المدخل غير صالح'
+        ]);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Units  $units
-     * @return \Illuminate\Http\Response
-     */
     public function show(Units $units)
     {
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Units  $units
-     * @return \Illuminate\Http\Response
-     */
     public function edit(Units $units)
     {
-        //
+        return Inertia::render('Units/Edit', [
+            'unit' => $units
+        ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Units  $units
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Units $units)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Units  $units
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Units $units)
     {
         //
