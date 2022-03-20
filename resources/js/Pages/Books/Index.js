c3fefdceb18c0ded2c4656002747f063b1d4aaf8
watan <drmody007.d0@gmail.com>
2022-03-20 18:35:06 +0300

    a

--- resources/js/Pages/Books/Index.js
+++ resources/js/Pages/Books/Index.js
@@ -1,7 +1,21 @@
+import Authenticated from "@/Layouts/Authenticated";
+import { Head } from "@inertiajs/inertia-react";
 import React from "react";
 
-const Index = () => {
-    return <div>Index</div>;
+const Index = ({ auth, errors }) => {
+    return (
+        <Authenticated
+            header={
+                <h2 className="font-semibold text-xl text-center text-gray-800 leading-tight">
+                    لوحة التحكم
+                </h2>
+            }
+            auth={auth}
+            errors={errors}
+        >
+            <Head title="لوحة التحكم" />
+        </Authenticated>
+    );
 };
 
 export default Index;
